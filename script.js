f6dc9bfa4fe880da44cd282a38745ec3c20aea84
Liudmyla Kozachenko <viergee@gmail.com>
2021-07-31 03:42:00 +0300

    Commit2

--- script.js
+++ script.js
@@ -20,13 +20,14 @@ const makeGalleryItemMarkup = item => {
 const makeGalleryMarkup = galleryItems
     .map(makeGalleryItemMarkup)
   .join('');
-console.log('makeGalleryMarkup', makeGalleryMarkup)
+// console.log('makeGalleryMarkup', makeGalleryMarkup)
     
 
 const refs = {
   galleryList: document.querySelector('.js-gallery'),
   modal: document.querySelector('.js-lightbox'),
   modalImage: document.querySelector('.lightbox__image'),
+  closeModalBtn: document.querySelector('[data-action="close-lightbox"]')
 }
  
 
@@ -39,13 +40,31 @@ function onGalleryListClick(e) {
   
     if (e.target.nodeName !== "IMG") {
         return;
-    }
-  refs.modal.classList.toggle('.is-open');
+  }
+  
+  refs.modal.classList.add('is-open');
   refs.modalImage.src = e.target.dataset.source;
-  console.log(refs.modalImage.src);
   refs.modalImage.alt = e.target.alt;
+  window.addEventListener('keydown', OnEscKeydown);
 }
 
-refs.galleryList.addEventListener('çlick', onGalleryListClick);
+function onCloseModal(e) {
+  refs.modal.classList.remove('is-open');
+  removeSrcImage();
+}
+
+function OnEscKeydown(e) {
+  if (e.key ==='Escape' ) {
+    onCloseModal();
+  }
+}
+
+function removeSrcImage() {
+  refs.modalImage.src = "";
+}
+
+refs.galleryList.addEventListener('click', onGalleryListClick);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+
 
 
